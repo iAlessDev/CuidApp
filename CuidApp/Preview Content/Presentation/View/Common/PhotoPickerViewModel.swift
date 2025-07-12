ae447e080be0ce04123fc5b8e4cec3a9a2f7b445
Paul Flores <paulfloresdev@icloud.com>
2025-07-11 19:19:13 -0600

    Feat: Photo picker view model

--- CuidApp/Preview Content/Presentation/View/Common/PhotoPickerViewModel.swift
+++ CuidApp/Preview Content/Presentation/View/Common/PhotoPickerViewModel.swift
@@ -5,4 +5,43 @@
 //  Created by Paul Flores on 10/07/25.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
+
+class PhotoPickerViewModel: ObservableObject {
+    
+    @Published var selectedPhoto: PhotosPickerItem?
+    @Published var image: UIImage?
+    @Published var selectedImageData: Data?
+    @Published var errorMessage: String?
+    
+    
+    
+    func loadSelectedPhoto() {
+        Task {
+            await withTaskGroup(of: (UIImage?, Error?).self) { taskGroup in
+                taskGroup.addTask {
+                    do {
+                        if
+                            let imageData = try await self.selectedPhoto?.loadTransferable(type: Data.self),
+                            let image = UIImage(data: imageData) {
+                            return (image, nil)
+                        }
+                        return (nil, nil)
+                    } catch {
+                        return (nil, nil)
+                    }
+                }
+                for await result in taskGroup {
+                    if result.1 != nil {
+                        errorMessage = "Failed to lead image"
+                        break
+                    } else if let image = result.0 {
+                        self.image = image
+                    }
+                    
+                }
+            }
+        }
+    }
+}
