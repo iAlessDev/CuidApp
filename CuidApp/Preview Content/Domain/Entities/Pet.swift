998b9b0c77196adfa491bad1e3134866e85e588b
Paul Flores <paulfloresdev@icloud.com>
2025-07-11 19:20:30 -0600

    Feat: main models creation

--- CuidApp/Preview Content/Domain/Entities/Pet.swift
+++ CuidApp/Preview Content/Domain/Entities/Pet.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Pet: Identifiable, Hashable {
     let id: UUID
@@ -14,7 +15,17 @@ struct Pet: Identifiable, Hashable {
     let name: String
     let birthDate: Date
     let isAlive: Bool
-    let image: Data?
+    let image: UIImage?
     let breed: String
     
+    init(id: UUID, ownerId: UUID, animal: String, name: String, birthDate: Date, isAlive: Bool, image: UIImage?, breed: String) {
+        self.id = id
+        self.ownerId = ownerId
+        self.animal = animal
+        self.name = name
+        self.birthDate = birthDate
+        self.isAlive = isAlive
+        self.image = image
+        self.breed = breed
+    }
 }
