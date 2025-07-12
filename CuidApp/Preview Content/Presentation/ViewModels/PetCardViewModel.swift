b96429e9ab9da47cb38b0c3db5ab8eb9500bf758
Paul Flores <paulfloresdev@icloud.com>
2025-07-11 19:10:23 -0600

    Feature/view model for pet cards

--- CuidApp/Preview Content/Presentation/ViewModels/PetCardViewModel.swift
+++ CuidApp/Preview Content/Presentation/ViewModels/PetCardViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+class PetCardViewModel: ObservableObject {
+    
+    let pet: Pet
+    
+    init(pet: Pet) {
+        self.pet = pet
+    }
+    
+    func petAge() -> Int {
+        let differenceFromPetBirthdayToNow = Calendar.current.dateComponents([.year], from: pet.birthDate, to: Date())
+        
+        return differenceFromPetBirthdayToNow.year!
+    }
+}
