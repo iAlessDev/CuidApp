7e817ab091a76b0fd33a420cdbc6664b81d23142
Paul Flores <paulfloresdev@icloud.com>
2025-07-11 19:20:13 -0600

    Feat: main models creation

--- CuidApp/Preview Content/Domain/Entities/User.swift
+++ CuidApp/Preview Content/Domain/Entities/User.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct User {
+    var id: UUID
+    var name: String
+    var email: String
+    var birthDate: Date
+    var pets: [Pet] = []
+    var profileImage: UIImage?
+
+    init(id: UUID, name: String, email: String, birthDate: Date, pets: [Pet], profileImage: UIImage?) {
+        self.id = id
+        self.name = name
+        self.email = email
+        self.birthDate = birthDate
+        self.pets = pets
+        self.profileImage = profileImage
+    }
+}
