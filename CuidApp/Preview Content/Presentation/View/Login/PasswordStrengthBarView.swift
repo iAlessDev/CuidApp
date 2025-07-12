a23dd7f41230b116d06be70c261f307db5b36144
Paul Flores <paulfloresdev@icloud.com>
2025-07-11 19:18:15 -0600

    Feat: password strength bar

--- CuidApp/Preview Content/Presentation/View/Login/PasswordStrengthBarView.swift
+++ CuidApp/Preview Content/Presentation/View/Login/PasswordStrengthBarView.swift
@@ -7,12 +7,53 @@
 
 import SwiftUI
 
-struct PasswordStrengthBarView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+import SwiftUI
+
+enum PasswordSecurityLevel {
+    case weak, medium, strong
+    
+    // Color asociado
+    var color: Color {
+        switch self {
+        case .weak:   return .red
+        case .medium: return .orange
+        case .strong: return .green
+        }
+    }
+    
+    // Cuántos segmentos se llenan (1, 2 o 3)
+    var filledSegments: Int {
+        switch self {
+        case .weak:   return 1
+        case .medium: return 2
+        case .strong: return 3
+        }
     }
 }
 
+struct PasswordStrengthBarView: View {
+    let strength: PasswordSecurityLevel
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            ForEach(0..<3) { index in
+                Rectangle()
+                    .frame(height: 3)
+                    .foregroundColor(segmentColor(at: index))
+            }
+        }
+        .cornerRadius(2.5)
+        .padding(.horizontal)
+    }
+    
+    private func segmentColor(at index: Int) -> Color {
+        return index < strength.filledSegments
+            ? strength.color
+            : Color.gray.opacity(0.3)
+    }
+}
+
+
 #Preview {
-    PasswordStrengthBarView()
+    PasswordStrengthBarView(strength: PasswordSecurityLevel.medium)
 }
