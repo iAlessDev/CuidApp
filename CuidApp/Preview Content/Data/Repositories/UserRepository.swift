c2168de75519d91568139a5172d35c8340528ad3
Paul Flores <paulfloresdev@icloud.com>
2025-07-11 19:20:53 -0600

    Feat: user repository

--- CuidApp/Preview Content/Data/Repositories/UserRepository.swift
+++ CuidApp/Preview Content/Data/Repositories/UserRepository.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class UserRepository: UserRepositoryProtocol {
+    func saveUser(_ user: User) async -> Bool {
+        return true
+    }
+    
+    func getUser() async -> User? {
+        return Optional.none
+    }
+    
+    func deleteUser() async -> Bool {
+        return true
+    }
+}
