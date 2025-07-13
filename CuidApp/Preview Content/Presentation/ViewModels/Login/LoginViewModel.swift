a7e4fb6ce1b98ceb1816c4e98ddf3e5acb961796
Paul Flores <paulfloresdev@icloud.com>
2025-07-13 01:04:54 -0600

    Feat: create login logic

--- CuidApp/Preview Content/Presentation/ViewModels/Login/LoginViewModel.swift
+++ CuidApp/Preview Content/Presentation/ViewModels/Login/LoginViewModel.swift
@@ -6,3 +6,90 @@
 //
 
 import Foundation
+
+class LoginViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var showAlert = false
+    @Published var alertMessage: String = ""
+    @Published var alertTitle: String = ""
+    
+    private let userRepository: UserRepositoryProtocol
+    private let credentialRepository: CredentialsRepositoryProtocol
+    private let session: SessionManager
+    
+    
+    init(
+        session: SessionManager,
+        userRepository: UserRepositoryProtocol = UserRepository(),
+        credentialRepository: CredentialsRepositoryProtocol = KeychainHelper())
+    {
+        self.session = session
+        self.userRepository = userRepository
+        self.credentialRepository = credentialRepository
+    }
+    
+    func signIn() async throws {
+        let user = try await fetchUser()
+        try verifyPassword()
+        completeLogin(with: user)
+      }
+    
+    private func fetchUser() async throws -> User {
+        guard let user = try await userRepository.fetchUserWithEmail(email) else {
+        throw LoginError.userNotFound
+            
+        }
+        return user
+    }
+    
+    private func verifyPassword() throws {
+        let stored = try credentialRepository.getPassword(for: email)
+        guard password == stored else {
+            throw LoginError.invalidCredentials
+        }
+    }
+    
+    private func completeLogin(with user: User) {
+        Task {
+            await session.login(with: user)
+        }
+    }
+    
+    func handle(error: Error) {
+        switch error {
+            
+        case let err as LoginError:
+          showAlert(title: "Login Error", message: err.errorDescription!)
+            
+        case let err as KeychainHelperError:
+          showAlert(title: "Keychain Error", message: err.messageError)
+            
+        default:
+          showAlert(title: "Error inesperado", message: error.localizedDescription)
+            
+        }
+    }
+
+    private func showAlert(title: String, message: String) {
+        alertTitle   = title
+        alertMessage = message
+        showAlert    = true
+    }
+}
+
+enum LoginError: LocalizedError {
+  case userNotFound
+  case invalidCredentials
+
+  var errorDescription: String? {
+      switch self {
+      case .userNotFound:
+          return "No se encontró un usuario con ese email."
+      case .invalidCredentials:
+          return "Email o contraseña incorrectos."
+      }
+    }
+}
+
+
