55afcb938b72543931e38dcee9b59b92ac3832f1
Paul Flores <paulfloresdev@icloud.com>
2025-07-14 13:32:05 -0600

    refactor: change User image type to Data and add SwiftData conversion method

--- CuidApp/Preview Content/Domain/Entities/User.swift
+++ CuidApp/Preview Content/Domain/Entities/User.swift
@@ -8,20 +8,34 @@
 import Foundation
 import SwiftUI
 
-struct User {
+struct User: ToSwiftDataUserProtocol {
+    
     var id: UUID
     var name: String
     var email: String
     var birthDate: Date
     var pets: [Pet] = []
-    var profileImage: UIImage?
+    var profileImage: Data?
 
-    init(id: UUID, name: String, email: String, birthDate: Date, pets: [Pet], profileImage: UIImage?) {
+    init(id: UUID, name: String, email: String, birthDate: Date, pets: [Pet], profileImage: Data?) {
         self.id = id
         self.name = name
         self.email = email
         self.birthDate = birthDate
         self.pets = pets
         self.profileImage = profileImage
+    }
+    
+    func toSwiftDataUser() -> SwiftDataUser {
+        let swiftDataUser =
+            SwiftDataUser(
+                id: id,
+                name: name,
+                email: email,
+                birthDate: birthDate,
+                pets: pets.map{ $0.toSwiftDataPet() },
+                profileImage: profileImage
+            )
+        return swiftDataUser
     }
 }
