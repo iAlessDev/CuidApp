84ed848806df983e17ff06a1b78675424daa16a9
Paul Flores <paulfloresdev@icloud.com>
2025-07-14 13:29:27 -0600

    Refactor: refactor code for clean code and add personalized errors

--- CuidApp/Preview Content/Data/Repositories/UserRepository.swift
+++ CuidApp/Preview Content/Data/Repositories/UserRepository.swift
@@ -6,14 +6,49 @@
 //
 
 import Foundation
+import SwiftData
 
 class UserRepository: UserRepositoryProtocol {
-    func saveUser(_ user: User) async -> Bool {
-        return true
+    
+    private let container: ModelContainer
+    private let context: ModelContext
+    
+    init() {
+        
+        self.container = try! ModelContainer(
+            for: SwiftDataUser.self,
+            configurations: ModelConfiguration(isStoredInMemoryOnly: false)
+        )
+        
+        self.context = ModelContext(container)
     }
     
-    func getUser() async -> User? {
-        return Optional.none
+    func saveUser(_ user: User, password: String) async throws -> Bool {
+        let swiftDataUser = user.toSwiftDataUser()
+        
+        do {
+            context.insert(swiftDataUser)
+            try context.save()
+            return true
+        } catch {
+            return error.localizedDescription.contains("already exists")
+        }
+    }
+    
+    func fetchUserWithEmail(_ email: String) async throws -> User? {
+        let predicate =
+        #Predicate<SwiftDataUser> { register in register.email == email }
+        
+        let descriptor = FetchDescriptor<SwiftDataUser>(predicate: predicate)
+        
+        do {
+            let swiftDataUser = try context.fetch(descriptor)
+            print(swiftDataUser.first?.toUser() ?? "No user")
+            return swiftDataUser.first?.toUser()
+        } catch {
+            print("Unable to fetch user")
+            return nil
+        }
     }
     
     func deleteUser() async -> Bool {
