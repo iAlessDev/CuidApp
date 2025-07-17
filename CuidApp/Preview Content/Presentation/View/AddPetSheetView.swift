963b0b6b97ee7489ece8ed471d87e641b164e1cb
Paul Flores <paulfloresdev@icloud.com>
2025-07-16 23:50:59 -0600

    feat: create a new sheet to add a pet and his view model

---
+++ CuidApp/Preview Content/Presentation/View/AddPetSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPetSheetView.swift
+//  CuidApp
+//
+//  Created by Paul Flores on 14/07/25.
+//
+
+import SwiftUI
+
+struct AddPetSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPetSheetView()
+}
---
+++ CuidApp/Preview Content/Presentation/ViewModels/AddPetSheetViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddPetSheetViewModel.swift
+//  CuidApp
+//
+//  Created by Paul Flores on 14/07/25.
+//
+
+import Foundation
